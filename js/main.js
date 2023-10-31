f6a26f74a96db3382cadc07da6eb710e385fcc2f
minhluc073 <nguyenminhluc0703@gmail.com>
2023-10-31 14:29:05 +0700

    update components

--- js/main.js
+++ js/main.js
@@ -22,19 +22,36 @@
   ------------------------------------------------------------------------------------- */
   var showPass = function () {
     $(".show-pass").on("click", function () {
-      $(this).toggleClass("active");
-      if ($(".password-field").attr("type") == "password") {
-        $(".password-field").attr("type", "text");
-      } else if ($(".password-field").attr("type") == "text") {
-        $(".password-field").attr("type", "password");
-      }
+        $(this).toggleClass("active");
+        if ($(".password-field").attr("type") == "password") {
+          $(".password-field").attr("type", "text");
+        } else if ($(".password-field").attr("type") == "text") {
+          $(".password-field").attr("type", "password");
+        }
     });
+
     $(".show-pass2").on("click", function () {
       $(this).toggleClass("active");
       if ($(".password-field2").attr("type") == "password") {
         $(".password-field2").attr("type", "text");
       } else if ($(".password-field2").attr("type") == "text") {
         $(".password-field2").attr("type", "password");
+      }
+    });
+    $(".show-pass3").on("click", function () {
+      $(this).toggleClass("active");
+      if ($(".password-field3").attr("type") == "password") {
+        $(".password-field3").attr("type", "text");
+      } else if ($(".password-field3").attr("type") == "text") {
+        $(".password-field3").attr("type", "password");
+      }
+    });
+    $(".show-pass4").on("click", function () {
+      $(this).toggleClass("active");
+      if ($(".password-field4").attr("type") == "password") {
+        $(".password-field4").attr("type", "text");
+      } else if ($(".password-field4").attr("type") == "text") {
+        $(".password-field4").attr("type", "password");
       }
     });
   };
@@ -234,6 +251,27 @@
     }
   };
 
+  /* image select
+  ------------------------------------------------------------------------------------- */
+  var selectImages = function () {
+    if ($(".image-select").length > 0) {
+      const selectIMG = $(".image-select");
+      selectIMG.find("option").each((idx, elem) => {
+        const selectOption = $(elem);
+        const imgURL = selectOption.attr("data-thumbnail");
+        if (imgURL) {
+          selectOption.attr(
+            "data-content",
+            "<img src='%i'/> %s"
+              .replace(/%i/, imgURL)
+              .replace(/%s/, selectOption.text())
+          );
+        }
+      });
+      selectIMG.selectpicker();
+    }
+  };
+
   /* active Suggestions
   ------------------------------------------------------------------------------------- */
   var activeSuggest = function () {
@@ -334,6 +372,7 @@
     pressToggle();
     activePhoto();
     activeSuggest();
+    selectImages();
     loadmore();
     preloader();
     themeSetting();
