f49f64ec5cb5dd9f44b7e630f824a22f8d9cbeee
minhluc073 <nguyenminhluc0703@gmail.com>
2023-11-05 16:59:43 +0700

    update range

--- js/rangle-slider.js
+++ js/rangle-slider.js
@@ -108,6 +108,100 @@
     }
   };
 
+  var rangeTwoKeypress = function () {
+    if ($("#keypress-two-val").length > 0) {
+      var skipSlider = document.getElementById("keypress-two-val");
+      var skipValues = [
+        document.getElementById("ip-val-lower"),
+        document.getElementById("ip-val-upper"),
+      ];
+
+      noUiSlider.create(skipSlider, {
+        start: [30, 70],
+        connect: true,
+        tooltips: [true, true],
+        step: 1,
+        range: {
+          min: 0,
+          max: 100,
+        },
+        format: {
+          from: function (value) {
+            return parseInt(value);
+          },
+          to: function (value) {
+            return parseInt(value);
+          },
+        },
+      });
+
+      skipSlider.noUiSlider.on("update", function (val, e) {
+        skipValues[e].value = val[e];
+      });
+    }
+  };
+
+  var rangeSoftLimit = function () {
+    if ($("#limit-val").length > 0) {
+      var limitVal = document.getElementById("limit-val");
+      noUiSlider.create(limitVal, {
+        start: 50,
+        range: {
+          min: 0,
+          max: 100,
+        },
+        pips: {
+          mode: "values",
+          values: [20, 80],
+          density: 5,
+        },
+      });
+      var limitHandle = limitVal.noUiSlider;
+      limitHandle.on("change", function (val, e) {
+        if (val[e] < 20) {
+          this.set(20);
+        } else if (val[e] > 80) {
+          this.set(80);
+        }
+      });
+    }
+  };
+  var rangePips = function () {
+    if (jQuery("#pips-val").length > 0) {
+      var pipsSlider = document.getElementById("pips-val");
+      noUiSlider.create(pipsSlider, {
+        range: {
+          min: 0,
+          max: 100,
+        },
+        start: [50],
+        pips: {
+          mode: "count",
+          values: 5,
+          density: 5,
+        },
+      });
+    }
+  };
+
+  var rangeDirection = function () {
+    if ($("#slider-dir").length > 0) {
+      var dirSlider = document.getElementById("slider-dir");
+      noUiSlider.create(dirSlider, {
+        start: 80,
+        range: {
+          min: 0,
+          max: 100,
+        },
+      });
+
+      var dirVal = document.querySelector(".field-dir");
+      dirSlider.noUiSlider.on("update", function (val, e) {
+        dirVal.innerHTML = val[e];
+      });
+    }
+  };
+
   var rangeOne = function () {
     if ($("#range-one-val").length > 0) {
       var directionSlider = document.getElementById("range-one-val");
@@ -174,5 +268,9 @@
     rangeSliderTwo();
     rangeSliderTooltip();
     rangeSliderDisable();
+    rangeTwoKeypress();
+    rangeSoftLimit();
+    rangePips();
+    rangeDirection();
   });
 })(jQuery);
